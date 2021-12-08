adb5f1db550bb30d5a30985bd5546b7ad778cedf
Miguel Pat <michaelapl@pop-os.localdomain>
2021-12-07 21:59:16 -0600

    added gem extension to gitignore, fixed MAL API route

--- lib/ruby_mal_client/configurations.rb
+++ lib/ruby_mal_client/configurations.rb
@@ -1,4 +1,4 @@
 module Configurations
     MAL_CLIENT_ID = "ab75fe72617c2d1ab411e46bb31c144f"
-    MAL_API_URL = "https://api.myanimelist.net/" 
+    MAL_API_URL = "https://api.myanimelist.net/v2/" 
 end
